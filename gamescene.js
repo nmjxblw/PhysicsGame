9bc3623c7778c753bee22b8358b4971e06cda165
nmjxblw <zchen386@ucsc.edu>
2023-05-09 15:23:36 -0700

    modified:   gamescene.js 	modified:   physicgame.js

--- gamescene.js
+++ gamescene.js
@@ -1,8 +1,12 @@
-class gameScene1 extends PhysicGameScene{
-    constructor(){
-        super("Scene1");
+class GameSceneExmaple extends PhysicGameScene {
+    constructor() {
+        super("Example1");
     }
 }
+
+var text1;
+var mouseX = 0;
+var mouseY = 0;
 
 class Menu extends PhysicGameScene {
 
@@ -11,26 +15,56 @@ class Menu extends PhysicGameScene {
     }
 
     create() {
-        this.add.text(this.w / 2, this.h / 2, "📺 This is the center.")
+        text1 = this.add.text(this.game.config.width / 2,
+            this.game.config.height / 2,
+            "📺 This is the center.")
             .setColor("#fff")
-            .setFontSize(13);
+            .setFontSize(50)
+            .setOrigin(0.5)
+            .setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
+
+        let FullScreenText = this.add.text(0, 0, "Full Screen")
+            .setFontSize(30)
+            .setOrigin(0.5)
+            .setPosition(1800, 1050)
+            .setAlpha(0.1)
+            .setInteractive()
+            .on("pointerdown", () => {
+                if (this.scale.isFullscreen) {
+                    this.scale.stopFullscreen();
+                } else {
+                    this.scale.startFullscreen();
+                }
+            })
+            .on("pointerover",()=>{
+                FullScreenText.setAlpha(1);
+            })
+            .on("pointerout",()=>{
+                FullScreenText.setAlpha(0.1);
+            });
 
         this.input.on("pointermove", (pointer) => {
-            console.log("centerX:" + this.w / 2);
-            console.log("centerY:" + this.h / 2);
+            console.log("centerX:" + this.game.config.width / 2);
+            console.log("centerY:" + this.game.config.height / 2);
             console.log("x:" + pointer.x);
             console.log("y:" + pointer.y);
-            if ((pointer.x <= this.w / 2 + 50
-                && pointer.x >= this.w / 2 - 50)
-                && (pointer.y <= this.h / 2 + 50
-                    && pointer.y >= this.h / 2 - 50)) {
+            console.log(this.game.globals.key);
+            mouseX = pointer.x;
+            mouseY = pointer.y;
+            if ((pointer.x <= this.game.config.width / 2 + 50
+                && pointer.x >= this.game.config.width / 2 - 50)
+                && (pointer.y <= this.game.config.height / 2 + 50
+                    && pointer.y >= this.game.config.height / 2 - 50)) {
                 console.log("In the center!");
             }
         });
     }
 
+    update() {
+        text1.setText("X: " + mouseX.toFixed(2) + " Y: " + mouseY.toFixed(2));
+    }
 }
- const game = new Phaser.Game({
+const game = new Phaser.Game({
     type: Phaser.AUTO,
     scale: {
         mode: Phaser.Scale.FIT,
@@ -40,5 +74,10 @@ class Menu extends PhysicGameScene {
     },
     scene: [Menu],
     backgroundColor: 0x000000,
-    title: "Physic Game"
+    title: "Physic Game",
 });
+
+game.globals = {
+    key: 100,
+    w: game.config.width,
+};
