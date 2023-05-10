caaa791f47311af0d6b4a15c5c7c50898ce2afd8
nmjxblw <zchen386@ucsc.edu>
2023-05-09 17:37:15 -0700

    new file:   assets/funny-meow-110120.mp3 	new file:   assets/gamemaker.png 	modified:   gamescene.js 	modified:   physicgame.js

--- gamescene.js
+++ gamescene.js
@@ -4,9 +4,86 @@ class GameSceneExmaple extends PhysicGameScene {
     }
 }
 
+class LogoScene extends Phaser.Scene{
+
+    constructor(){
+        super("logo");
+    }
+
+    preload() {
+        this.load.path = './assets/';
+        this.load.image("gamemaker", "gamemaker.png");
+        this.load.audio('meow', 'funny-meow-110120.mp3');
+    }
+
+    create() {
+
+        const centerX = this.game.config.width / 2;
+        const centerY = this.game.config.height / 2;
+        var rotateAngle = 0;
+
+        this.cameras.main.fadeIn(1000, 255, 255, 255);
+
+        let timer = this.time.addEvent({
+            delay: 80,
+            callback: createCircle,
+            callbackScope: this,
+            loop: true
+        });
+
+        function createCircle() {
+            let color = Phaser.Display.Color.RandomRGB(50, 255);
+
+            rotateAngle += 0.23;
+
+            let circle = this.add.circle(centerX + 150 * Math.cos(rotateAngle), centerY + 150 * Math.sin(rotateAngle), 13, color.color);
+
+            this.tweens.add({
+                targets: circle,
+                alpha: 0,
+                scale: 2.2,
+                duration: 2500,
+                onComplete: function () {
+                    circle.destroy();
+                }
+            });
+        }
+
+        var logoImage = this.add.image(
+            centerX,
+            centerY,
+            'gamemaker'
+        )
+        logoImage.setScale(0.1);
+        logoImage.alpha = 0;
+
+        this.tweens.add({
+            targets: logoImage,
+            alpha: 1,
+            scaleX: 0.4,
+            scaleY: 0.4,
+            angle: -10,
+            duration: 1000,
+            ease: "liner"
+        });
+
+        const logoBgm = this.sound.add('meow', { loop: false });
+
+        logoBgm.play();
+
+        this.input.on('pointerdown', () => {
+            this.time.delayedCall(1000, () => {
+                this.cameras.main.fadeOut(1000, 255, 255, 255);
+                this.time.delayedCall(2000, () => {
+                    this.scene.start('Menu');
+                });
+            });
+        });
+    }
+}
+
+
 var text1;
-var mouseX = 0;
-var mouseY = 0;
 
 class Menu extends PhysicGameScene {
 
@@ -14,54 +91,18 @@ class Menu extends PhysicGameScene {
         super("Menu", "Menu")
     }
 
-    create() {
-        text1 = this.add.text(this.game.config.width / 2,
-            this.game.config.height / 2,
+    onEnter() {
+         text1 = this.add.text(this.w / 2,
+            this.h / 2,
             "📺 This is the center.")
-            .setColor("#fff")
+            .setColor("#fff000")
             .setFontSize(50)
             .setOrigin(0.5)
-            .setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
-
-        let FullScreenText = this.add.text(0, 0, "Full Screen")
-            .setFontSize(30)
-            .setOrigin(0.5)
-            .setPosition(1800, 1050)
-            .setAlpha(0.1)
-            .setInteractive()
-            .on("pointerdown", () => {
-                if (this.scale.isFullscreen) {
-                    this.scale.stopFullscreen();
-                } else {
-                    this.scale.startFullscreen();
-                }
-            })
-            .on("pointerover",()=>{
-                FullScreenText.setAlpha(1);
-            })
-            .on("pointerout",()=>{
-                FullScreenText.setAlpha(0.1);
-            });
-
-        this.input.on("pointermove", (pointer) => {
-            console.log("centerX:" + this.game.config.width / 2);
-            console.log("centerY:" + this.game.config.height / 2);
-            console.log("x:" + pointer.x);
-            console.log("y:" + pointer.y);
-            console.log(this.game.globals.key);
-            mouseX = pointer.x;
-            mouseY = pointer.y;
-            if ((pointer.x <= this.game.config.width / 2 + 50
-                && pointer.x >= this.game.config.width / 2 - 50)
-                && (pointer.y <= this.game.config.height / 2 + 50
-                    && pointer.y >= this.game.config.height / 2 - 50)) {
-                console.log("In the center!");
-            }
-        });
+            .setPosition(this.cx, this.cy);
     }
 
     update() {
-        text1.setText("X: " + mouseX.toFixed(2) + " Y: " + mouseY.toFixed(2));
+        text1.setText("X: " + this.pointerX.toFixed(2) + " Y: " + this.pointerY.toFixed(2));
     }
 }
 const game = new Phaser.Game({
@@ -72,12 +113,12 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Menu],
+    scene: [LogoScene,Menu],
     backgroundColor: 0x000000,
     title: "Physic Game",
 });
 
 game.globals = {
-    key: 100,
-    w: game.config.width,
+    TestVar: 100,
+
 };
--- physicgame.js
+++ physicgame.js
@@ -7,9 +7,89 @@ class PhysicGameScene extends Phaser.Scene {
     }
 
     create() {
+        this.transitionDuration = 1000;
+
+        this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
+
         this.w = this.game.config.width;
         this.h = this.game.config.height;
-        console.log("centerX:" + this.w / 2);
-        console.log("centerY:" + this.h / 2);
+
+        this.cx = this.cameras.main.centerX;
+        this.cy = this.cameras.main.centerY;
+
+        this.pointerX = 0;
+        this.pointerY = 0;
+
+        this.add.rectangle(this.w / 2, 10, this.w, 20).setFillStyle(0Xffffff);
+
+        let FullScreenText = this.add.text(0, 0, "Full Screen")
+            .setColor(0xff0000)
+            .setFontSize(30)
+            .setOrigin(0.5)
+            .setPosition(100, 10)
+            .setAlpha(0.5)
+            .setInteractive()
+            .on("pointerdown", () => {
+                if (this.scale.isFullscreen) {
+                    this.scale.stopFullscreen();
+                    FullScreenText.setText("Full Screen").setPosition(100, 10);
+                } else {
+                    this.scale.startFullscreen();
+                    FullScreenText.setText("Quit FullScreen").setPosition(140, 10);
+                }
+            })
+            .on("pointerover", () => {
+                FullScreenText.setAlpha(1);
+            })
+            .on("pointerout", () => {
+                FullScreenText.setAlpha(0.5);
+            });
+
+            let QuitText = this.add.text(0, 0, "Quit Game")
+            .setColor(0xff0000)
+            .setFontSize(30)
+            .setOrigin(0.5)
+            .setPosition(this.w-100, 10)
+            .setAlpha(0.5)
+            .setInteractive()
+            .on("pointerdown", () => {
+                window.close();
+            })
+            .on("pointerover", () => {
+                QuitText.setAlpha(1);
+            })
+            .on("pointerout", () => {
+                QuitText.setAlpha(0.5);
+            });
+
+        this.input.on("pointermove", (pointer) => {
+            //console.log("centerX:" + this.game.config.width / 2);
+            //console.log("centerY:" + this.game.config.height / 2);
+            /* console.log("x:" + pointer.x);
+            console.log("y:" + pointer.y); */
+            //console.log(this.game.globals.TestVar);//调用全局值
+            this.pointerX = pointer.x;
+            this.pointerY = pointer.y;
+            /* if ((pointer.x <= this.game.config.width / 2 + 50
+                && pointer.x >= this.game.config.width / 2 - 50)
+                && (pointer.y <= this.game.config.height / 2 + 50
+                    && pointer.y >= this.game.config.height / 2 - 50)) {
+                console.log("In the center!");
+            } */
+        });
+
+        this.onEnter();
+    }
+
+    gotoScene(key) {
+        console.log(key);
+        this.cameras.main.fade(this.transitionDuration, 0, 0, 0);
+        this.time.delayedCall(this.transitionDuration, () => {
+            this.scene.start(key);
+        });
+    }
+
+    onEnter() {
+        console.warn('This AdventureScene did not implement onEnter():', this.constructor.name);
     }
 }
